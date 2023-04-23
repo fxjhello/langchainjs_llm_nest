130808ef65c8118dd72ec02f2026430630534532
fxjhello <2395112364@qq.com>
2023-04-23 10:51:25 +0800

    app修改

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -3,18 +3,23 @@ import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-
-import { RetrievalQAChain } from 'langchain/chains';
-import { ChatGlm6BLLm } from '../llms/chatglm_6b_llm';
+import {ChatGlm6BLLM} from '../chat_models/chatglm-6b'
+import { LLMChain, RetrievalQAChain } from 'langchain/chains';
+//import { ChatGlm6BLLm } from '../llms/chatglm_6b_llm';
 import { T2VLargeChineseEmbeddings } from '../embeddings/text2vec-large-chinese.embedding';
 import { CohereEmbeddings } from "langchain/embeddings/cohere";
+import {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate,
+} from "langchain/prompts";
 // import { OpenAI } from 'langchain/llms/openai';
 
 @Injectable()
 export class AppService {
   async run() {
     // Create docs with a loader
-    const model = new ChatGlm6BLLm({});
+    //const model = new ChatGlm6BLLm({});
     const loader = new DirectoryLoader(
       "./fileUpload",
       {
@@ -34,7 +39,7 @@ export class AppService {
 
     const vectorStore = await HNSWLib.fromDocuments(
       docs,
-      new T2VLargeChineseEmbeddings()
+      new CohereEmbeddings({apiKey:'UQhKlkpjHhEQkMszkxdTmoH4Ioh7Zo8cxvCGxSYF'})
     );
     
     const directory = './fileProcessing';
@@ -43,11 +48,27 @@ export class AppService {
     // Load the vector store from the same directory
     const loadedVectorStore = await HNSWLib.load(
       directory,
-      new T2VLargeChineseEmbeddings()
+      new CohereEmbeddings({apiKey:'UQhKlkpjHhEQkMszkxdTmoH4Ioh7Zo8cxvCGxSYF'})
     );
 
     const result = await loadedVectorStore.similaritySearch("hello world", 1);
   console.log(result);
+  //根据内容回答问题
+  const chat = new ChatGlm6BLLM({ temperature: 0.01 });
+  const translationPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      `使用以下文段, 用中文回答用户问题。如果无法从中得到答案，请说'没有足够的相关信息'。已知内容:${result[0].pageContent}`
+    ),
+    HumanMessagePromptTemplate.fromTemplate("{text}"),
+  ]);
+  const chain = new LLMChain({
+    prompt: translationPrompt,
+    llm: chat,
+  });
+  const responseB = await chain.call({
+    text: "东大开江成立于什么时候",
+  });
+  console.log(responseB);
     // Search for the most similar document
     /* const chain = RetrievalQAChain.fromLLM(
       model,
